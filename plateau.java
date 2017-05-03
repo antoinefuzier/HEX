ada9414fe4f0fa5dc0a4fe2fe489ff1bb4d43411
antoinefuzier <antoinefuzier@hotmail.fr>
2017-05-03 14:05:22 +0200

    Create plateau.java

---
+++ plateau.java
@@ -0,0 +1,70 @@
+package Hex;
+
+public class Plateau{
+
+	private final int MAX=1000;
+	private char hexa[]=new char[MAX];
+	private int taille;
+	
+	public Plateau(int taille){
+		
+		this.taille=taille;
+		for(int i=0;i<taille*taille;i++)
+			hexa[i]='.';	
+	}
+	
+	public void initialiser(){
+		
+		for(int i=0;i<taille*taille;i++)
+			hexa[i]='.';
+		
+	}
+	
+	void afficher(){
+		int h=0;
+		for(int j=0;j<=taille+1;j++){	/* boucle correspondant au nombre de ligne à afficher*/
+			
+			for(int a=0;a<j+1;a++)
+				System.out.print(" ");
+			
+			if(j==0){
+				/*  affichage de la première ligne 	*/
+				for(int i=0;i<=taille;i++)
+						System.out.print("W ");
+			
+				System.out.println("B");
+			}
+			
+			
+			/* affichage du centre */
+			else if(j>0 && j<=taille){
+				
+				for(int x=0;x<=taille+1;x++){
+					if(x==0 || x==taille+1)
+						System.out.print("B ");
+					else if(x>0 && x<taille+1){
+						System.out.print(hexa[h] + " ");
+						h++;
+					}
+						
+				}
+				System.out.println("");
+			}
+			else{ /* affichage dernière ligne */
+				
+				System.out.print("B ");
+				for(int i=0;i<=taille;i++)
+					System.out.print("W ");
+		
+			
+				
+			}
+			
+			
+			
+		}
+		
+	}
+	
+}
+
